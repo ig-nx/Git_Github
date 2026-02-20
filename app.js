eeb8c789a15ae34478e907de6bde7ff7531358d5
Ignacio <ignacio.le96@gmail.com>
2026-02-20 00:26:08 -0300

    modificacion de js en boton

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 function cambiarTexto() {
     const p = document.getElementById("mensaje");
     p.textContent = "Git está funcionando correctamente 🚀";
+
+    const button = document.querySelector("button");
+    button.disabled = true;
 }
